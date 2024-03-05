1b85beaf40121677a69c3a0ec86a57176989661d
Dženan Mecinović <mecinovicdz@gmail.com>
2024-03-05 17:16:02 +0100

    passing data with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,18 +15,8 @@ export class RecipeListComponent {
       'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
     ),
     new Recipe(
-      'A Test Recipe 2',
+      'A Second Test Recipe',
       'This is simply a test 2',
-      'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe 3',
-      'This is simply a test 3',
-      'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe 4',
-      'This is simply a test 4',
       'https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
     ),
   ];
