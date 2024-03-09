31c519ede4b72f9d79ca4b84cf4fa8ecc1adae48
Dženan Mecinović <mecinovicdz@gmail.com>
2024-03-09 20:55:56 +0100

    adding ingredients from recipes to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css',
+  providers: [ShoppingListService],
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit() {}
+
+  toShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
